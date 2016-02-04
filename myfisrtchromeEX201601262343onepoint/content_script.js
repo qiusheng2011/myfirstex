1db8ab43f61506b85afd701a79a78f3b37cbcbae
qiusheng2011 <137812291@qq.com>
2016-02-04 11:48:44 +0800

    232
    232

--- myfisrtchromeEX201601262343onepoint/content_script.js
+++ myfisrtchromeEX201601262343onepoint/content_script.js
@@ -238,7 +238,7 @@ function ShowInputWindow(state) {
     openconfirm.innerHTML="function openConfrim(id){"+"if(id.name==\"OFF\"){id.name=\"ON\";id.innerHTML=\"pick up  web ON\";id.style.background=\"#61CE3C\";}else{id.name=\"OFF\";id.innerHTML=\"pick up  web OFF\";id.style.background=\"#BD2D30\";}}";
      suspensionWindow.appendChild(openconfirm);
 
-    var suspensionwindowStyleContent="#suspensionwindow{width:380px;background:rgba(0,0,0,0.6);color:#fff;position:fixed;right:10px;top:60px;font:400 12px/1.2 Helvetica;overflow:hidden;z-index:99999;border-radius:5px;padding:5px;margin: 0 0 0 5px;max-height:90vh;overflow-Y:auto;}";
+    var suspensionwindowStyleContent="#suspensionwindow{width:380px;background:rgba(0,0,0,0.6);color:#fff;position:fixed;right:10px;top:60px;font:400 12px/1.2 Helvetica;overflow:hidden;z-index:99999;border-radius:5px;padding:5px;margin: 0 0 0 5px;max-height:80vh;overflow-Y:auto;}";
     suspensionwindowStyleContent+="#suspensionwindow .line{overflow:hidden;margin-bottom:10px;}";
     suspensionwindowStyleContent+="#suspensionwindow button{width:50px;height:18px;left:5px; line-height:18px;background:#f47737;color:#fff;border-radius:4px;margin-left:5px;border:none; padding:0;margin:0 0 0 5px;}";
     suspensionwindowStyleContent+="#suspensionwindow p.sw{width:160px;float:left;text-align:left;margin:0;line-height:18px;}";
@@ -301,7 +301,7 @@ function ShowInputWindow(state) {
     }
     else {
         button.setAttribute("name","OFF");
-        susdiv.style.maxHeight="90vh";
+        susdiv.style.maxHeight="80vh";
         susdiv.style.height="";
          susdiv.style.overflowY="auto";
     }
@@ -310,8 +310,12 @@ function ShowInputWindow(state) {
 
 }
 
-
+// 拖拽
+    startDrag(suspensionWindow,suspensionWindow);
 }
+
+
+
 
 //---------------------------事件方法-----------------
 
@@ -1116,16 +1120,70 @@ function  analyseDomChange(mutations)
 
 
 
-// ------------------------ 监听分析-------------------------
-//------------------------ 鼠标 拖动
+// ------------------------ 监听分析E-------------------------
+//------------------------ 鼠标 拖动S----------------------
 
-var xl;
-var yl;
-function  moveSWDIV(e)
-{
+var params = {
+    left: 0,
+    top: 0,
+    currentX: 0,
+    currentY: 0,
+    flag: false
+};
+//获取相关CSS属性
 
+var getCss = function(o,key){
+    return o.currentStyle? o.currentStyle[key] : document.defaultView.getComputedStyle(o,false)[key];
+};
 
-}
+function startDrag(bar, target, callback){
+    if(getCss(target, "left") !== "auto"){
+        params.left = getCss(target, "left");
+    }
+    else
+    {
+        params.left=target.offsetLeft;
+    }
+    if(getCss(target, "top") !== "auto"){
+        params.top = getCss(target, "top");
+    }
+    //o是移动对象
+    bar.onmousedown = function(event){
+        params.flag = true;
+        if(!event){
+            event = window.event;
+            //防止IE文字选中
+            bar.onselectstart = function(){
+                return false;
+            }
+        }
+        var e = event;
+        params.currentX = e.clientX;
+        params.currentY = e.clientY;
+    };
+    document.onmouseup = function(){
+        params.flag = false;
+        if(getCss(target, "left") !== "auto"){
+            params.left = getCss(target, "left");
+        }
+        if(getCss(target, "top") !== "auto"){
+            params.top = getCss(target, "top");
+        }
+    };
+    document.onmousemove = function(event){
+        var e = event ? event: window.event;
+        if(params.flag){
+            var nowX = e.clientX, nowY = e.clientY;
+            var disX = nowX - params.currentX, disY = nowY - params.currentY;
+            target.style.left = parseInt(params.left) + disX + "px";
+            target.style.top = parseInt(params.top) + disY + "px";
+        }
+
+        if (typeof callback == "function") {
+            callback(parseInt(params.left) + disX, parseInt(params.top) + disY);
+        }
+    }
+};
 
 
 // 其他方法
